556a740d6d3481a9f252ed7cbab6aa0cf83c720d
dalhatmd <umarwalid4055@gmail.com>
2025-04-19 22:19:06 +0100

    Update README

--- backend/Atlas_api/Atlas_api/settings.py
+++ backend/Atlas_api/Atlas_api/settings.py
@@ -29,6 +29,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Email settings
+
+EMAIL-BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+
 
 # Application definition
 
