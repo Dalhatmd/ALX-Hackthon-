e686c7a9ae345a6e70a913a880b5981649859816
dalhatmd <umarwalid4055@gmail.com>
2025-04-21 17:22:02 +0100

    feat: user can search for multiple amenities

--- backend/Atlas_api/workspaces/filters.py
+++ backend/Atlas_api/workspaces/filters.py
@@ -1,14 +1,17 @@
-# filters.py
 import django_filters
 from .models import Workspace
+from django.db.models import Q
 
 class WorkspaceFilter(django_filters.FilterSet):
-    amenities = django_filters.CharFilter(method='filter_by_amenity_name')
+    amenities = django_filters.CharFilter(method='filter_amenities')
     location = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Workspace
         fields = ['location', 'is_available', 'workspace_type', 'min_capacity', 'max_capacity']
 
-    def filter_by_amenity_name(self, queryset, name, value):
-        return queryset.filter(amenities__name__icontains=value)
+    def filter_amenities(self, queryset, name, value):
+        amenities_list = [v.strip() for v in value.split(',')]
+        for amenity_name in amenities_list:
+            queryset = queryset.filter(amenities__name__icontains=amenity_name)
+        return queryset
